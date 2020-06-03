09300d19aff58498e370758285825797e23a79ef
ddiiwoong <ddiiwoong@gmail.com>
2020-06-03 23:59:21 +0900

    Move Files

---
+++ etc/hello.py
@@ -0,0 +1,13 @@
+a = 2
+b = 3
+
+first_name = 'jinwoong'
+last_name = 'Kim'
+
+a_list = ['사과','감','배']
+a_list.append('수박')
+
+a_dict = {'name':'bob','age':24}
+
+a_dict['fruits'] = a_list
+print(a_dict)
