1e4d6ed1350ebde1622c3c2f12e8a08e95b4199a
ddiiwoong <ddiiwoong@gmail.com>
2020-06-16 00:47:37 +0900

    Add Homework #4

--- homework/hw4/app.py
+++ homework/hw4/app.py
@@ -12,17 +12,29 @@ db = client.dbsparta                      # 'dbsparta'라는 이름의 db를 만
 def home():
     return render_template('index.html')
 
-## API 역할을 하는 부분
+@app.route('/orders', methods=['POST'])
+def write_orders():
+    orderName_receive = request.form['orderName_give']
+    orderSelect_receive = request.form['orderSelect_give']
+    orderAddress_receive = request.form['orderAddress_give']
+    orderPhone_receive = request.form['orderPhone_give']
+
+    order = {
+      'orderName': orderName_receive,
+      'orderSelect': orderSelect_receive,
+      'orderAddress': orderAddress_receive,
+      'orderPhone': orderPhone_receive
+    }
+
+    db.orders.insert_one(order)
+
+    return jsonify({'result': 'success', 'msg': '주문이 완료되었습니다.'})
 
 
-@app.route('/reviews', methods=['POST'])
-def write_review():
-    return jsonify({'result': 'success', 'msg': '이 요청은 POST!'})
-
-
-@app.route('/reviews', methods=['GET'])
-def read_reviews():
-    return jsonify({'result': 'success', 'msg': '이 요청은 GET!'})
+@app.route('/orders', methods=['GET'])
+def read_orders():
+    orders = list(db.orders.find({},{'_id':0}))
+    return jsonify({'result': 'success', 'orders': orders})
 
 
 if __name__ == '__main__':
